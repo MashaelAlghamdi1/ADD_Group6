5e3cf631471ffbd5b861b681b16ff237f6958f23
mashael_alghamdi <malghamdi1768@stu.kau.edu.sa>
2024-05-18 03:27:17 +0300

    Mashael commit

--- src/LaboratorySpecialist.java
+++ src/LaboratorySpecialist.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +13,31 @@
  * @author HC-atiaf
  */
 public class LaboratorySpecialist {
-    
+        private String name;
+    private String specialization;
+    private List<TestResult> testResults; // Association with TestResult
+
+    public LaboratorySpecialist(String name, String specialization) {
+        this.name = name;
+        this.specialization = specialization;
+
+    }
+
+    public LaboratorySpecialist() {
+
+    }
+
+    public void sddresult(Patient patient){
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("_______________________-");
+        patient.myInfo();
+        System.out.print("enter result:");
+        String result=scanner.nextLine();
+        TestResult testResult=new TestResult();
+        testResult.setResult(result);
+        patient.setTestResults(testResult);
+    System.out.println("______________________");
+    System.out.println("dun Test Results added successfully");
+
+}
 }
