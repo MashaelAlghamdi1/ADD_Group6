6340c026c731c46aa2417634e431da8481766191
mashael_alghamdi <malghamdi1768@stu.kau.edu.sa>
2024-05-18 03:21:59 +0300

    Mashael commit

--- src/Appointment.java
+++ src/Appointment.java
@@ -9,5 +9,37 @@
  * @author HC-atiaf
  */
 public class Appointment {
-    
+        private int appointmentId;
+    private String date;
+    private String state;
+    private Receptionist receptionist; // Association with Receptionist
+    private Patient patient; // Association with Patient
+
+    public Appointment(int appointmentId, String date, Receptionist receptionist, Patient patient) {
+        this.appointmentId = appointmentId;
+        this.date = date;
+        this.receptionist = receptionist;
+        this.patient = patient;
+    }
+    public int getAppointmentId() {
+        return appointmentId;
+    }
+    public String getState(){
+        return this.state;
+    }
+    public void setState(String state){
+        this.state=state;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public Appointment() {
+
+    }
 }
